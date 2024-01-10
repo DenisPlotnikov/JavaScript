dc18532b6aaa710e7c8179fc9f057e880bfda4d3
DenisPlotnikov <denisplotnikov7500@gmail.com>
2024-01-10 15:41:12 +0300

    commit

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log(11); //как число
+console.log("11"); // как строка
+console.log("Добро " + "пожаловать " + "на курс " + "Денис");
+//alert('10.01.2024');
+const div = document.getElementById("one");
+div.innerHTML += "Привет мир";
+
